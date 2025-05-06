9df1628fb7aef972406b58078bacc8a541595cca
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-05-06 12:08:26 +0100

    about.php - plan about page content

---
+++ about.php
@@ -0,0 +1,19 @@
+<?php require "template-parts/inc/header.php"; ?>
+
+
+    <main class="interface-container">
+
+        <!-- About Page 
+         
+            - About the app
+            - Techologies Used
+            - Development/Features
+            - Contact
+            - Links
+
+        -->
+            
+    </main>
+
+
+<?php require "template-parts/inc/footer.php"; ?>
