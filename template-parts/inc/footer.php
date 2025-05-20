ed03a1f48b5a8988233a87f40c49d3099e63f92a
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-05-20 10:31:19 +0100

    app v1.0.6

--- template-parts/inc/footer.php
+++ template-parts/inc/footer.php
@@ -3,7 +3,7 @@
     <footer>
 
 
-        <p>&copy; <?php echo date('Y'); ?> "Snooker Scorer App". By Jonnie Grieve Digital Media. <a href="https://github.com/jg-digital-media/jgdm_snooker_scorer" target="_blank">Repository</a> <a href="about.php"><span id="app_versioning">v1.0.5</span></a></p>
+        <p>&copy; <?php echo date('Y'); ?> "Snooker Scorer App". By Jonnie Grieve Digital Media. <a href="https://github.com/jg-digital-media/jgdm_snooker_scorer" target="_blank">Repository</a> <a href="about.php"><span id="app_versioning">v1.0.6</span></a></p>
 
     </footer>
 
