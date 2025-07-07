9a1c801986d1658854ad1bc616f02863ccae838f
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-07-07 13:34:31 +0100

    add confirmation modal template part

---
+++ assets/template-parts/part-rerack-modal.php
@@ -0,0 +1,12 @@
+    <div id="rerackModalOverlay" class="modal-overlay">
+        <div class="confirm-modal">
+            <div class="modal-title">Re-rack Table</div>
+            <div class="modal-message">
+                Really re-rack the table? This will reset the entire frame and no result will be recorded.
+            </div>
+            <div class="modal-buttons">
+                <button class="modal-btn cancel" id="rerackModalCancel">Cancel</button>
+                <button class="modal-btn confirm" id="rerackModalConfirm">Yes, Re-rack</button>
+            </div>
+        </div>
+    </div> 
